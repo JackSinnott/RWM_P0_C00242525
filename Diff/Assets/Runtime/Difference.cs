752e4b9ae69b0636f74f6cfd64f1e5c4f8521073
JackSinnott <c00242525@itcarlow.ie>
2022-09-19 10:06:20 +0100

    Removed all references to CubeFilter

--- Diff/Assets/Runtime/Difference.cs
+++ Diff/Assets/Runtime/Difference.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeFilter : MonoBehaviour
+public class Difference : MonoBehaviour
 {
     public static int[] Diff(int[] xs)
     {
--- Diff/Assets/Tests/Difference.cs
+++ Diff/Assets/Tests/Difference.cs
@@ -9,7 +9,7 @@ namespace Tests
         public void CubeFilterSimple()
         {
             int[] input = { 4, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
-            int[] output = CubeFilter.Diff(input);
+            int[] output = Difference.Diff(input);
             int[] expected = { 4, 2, 6, 5, 6, 5, 6, 5, 6, 5 };
 
             CollectionAssert.AreEqual(expected, output);
